978ab8a71bc69dab651ab178a37e6711dc43171a
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-08-25 20:12:44 -0400

    Add test api route

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 8081;
 
 const app = express();
+
+const testData = [
+  {
+    name: 'Tomek',
+    role: 'Admin',
+  },
+];
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -12,6 +19,10 @@ app.get('/', (req, res) => {
 
 app.get('/test', (req, res) => {
   res.send('Hello from fieldist test route!');
+});
+
+app.get('/api/test', (req, res) => {
+  res.json(testData);
 });
 
 app.listen(PORT, () => {
